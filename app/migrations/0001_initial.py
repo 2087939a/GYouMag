19305e75e7e99b0607f7ecf5a8e8d3fb2c2097fe
Terence Aqachmar <2087939a@student.gla.ac.uk>
2015-06-24 16:29:41 +0200

    24th of June

---
+++ app/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=128)),
+                ('content', models.TextField()),
+                ('views', models.IntegerField(default=0)),
+                ('pub_date', models.DateTimeField(verbose_name=b'date published')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=128)),
+                ('views', models.IntegerField(default=0)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='category',
+            field=models.ForeignKey(to='app.Category'),
+        ),
+    ]
