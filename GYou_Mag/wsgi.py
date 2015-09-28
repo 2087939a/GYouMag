3396ac5a2d878e0d06c0618e0c04d810ea1e64b2
Terence Aqachmar <terence.aqachmar@gmail.com>
2015-09-28 14:30:29 +0000

    rollback

--- GYou_Mag/wsgi.py
+++ GYou_Mag/wsgi.py
@@ -1,24 +1,11 @@
-# TURN ON THE VIRTUAL ENVIRONMENT FOR YOUR APPLICATION
-activate_this = '/home/gyoumagazine/.virtualenvs/app/bin/activate_this.py'
-execfile(activate_this, dict(__file__=activate_this))
 import os
-import sys
 
-# ADD YOUR PROJECT TO THE PYTHONPATH FOR THE PYTHON INSTANCE
-path = '/home/gyoumagazine/GYouMag/'
-if path not in sys.path:
-    sys.path.append(path)
+ 
+from django.core.wsgi import get_wsgi_application
 
-# IMPORTANTLY GO TO THE PROJECT DIR
-os.chdir(path)
-
-# TELL DJANGO WHERE YOUR SETTINGS MODULE IS LOCATED
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GYou_Mag.settings')
-
+ 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GYou_Mag.settings")
 # IMPORT THE DJANGO SETUP - NEW TO 1.7
-import django
-django.setup()
 
-# IMPORT THE DJANGO WSGI HANDLER TO TAKE CARE OF REQUESTS
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+
+application = get_wsgi_application()
